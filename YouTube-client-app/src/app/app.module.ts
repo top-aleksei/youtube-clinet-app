c2c9924da0829e56b3d0065b43a15d40e9a9c47e
Aleksei <toporov.aleksei@gmail.com>
2023-03-14 14:29:14 +0300

    fix: add missed files to structure

--- YouTube-client-app/src/app/app.module.ts
+++ YouTube-client-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchResultsComponent } from './search/search-results/search-results/search-results.component';
+import { SearchItemComponent } from './search/search-item/search-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    HeaderComponent,
+    SearchResultsComponent,
+    SearchItemComponent
   ],
   imports: [
     BrowserModule,
