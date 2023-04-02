5b7133b7b5701cd0ead70519e4638725751d1477
Aleksei <toporov.aleksei@gmail.com>
2023-04-02 19:39:06 +0300

    refactor: remove usless code

--- YouTube-client-app/src/app/core/guards/auth.guard.ts
+++ YouTube-client-app/src/app/core/guards/auth.guard.ts
@@ -7,11 +7,7 @@ import { AuthService } from '../../auth/services/auth.service';
 })
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
-  // canActivate(
-  //   route: ActivatedRouteSnapshot,
-  //   state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-  //   return true;
-  // }
+
   canActivate(): boolean {
     if (this.authService.userName === null) {
       this.router.navigate(['/auth']);
